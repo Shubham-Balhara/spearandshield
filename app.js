f726ffaadaf8376ada3486ebcd6aba323bb53fff
sbalhara <sbalhara007@gmail.com>
2018-05-06 17:22:16 +0530

    first coomit

---
+++ app.js
@@ -0,0 +1,53 @@
+var express=require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname+ '/public'));
+
+app.get('/', function(req, res) {
+   res.sendfile('index.html');
+});
+
+var count=1;
+var roomno =1;
+var users= 0;
+io.on('connection',function(socket){
+	var id = 'usr'+count;
+	count++;
+	
+    socket.join("room-"+roomno);
+    users++;
+    io.sockets.in("room-"+roomno).emit('connectToRoom',{id:id,roomid:"room-"+roomno,users:users});
+    if(io.nsps['/'].adapter.rooms["room-"+roomno] && io.nsps['/'].adapter.rooms["room-"+roomno].length > 1){
+    roomno++;
+    users=0;
+  }
+ 
+    socket.on('spear',function(data){
+    	socket.broadcast.in(data.obj.room_id).emit('attack',{value:10,obj:data.obj});
+    });
+
+    socket.on('result',function(data){
+      socket.broadcast.in(data.obj.room_id).emit('recieved',{value:10});
+    });
+
+    socket.on('alert',function(data){
+      socket.broadcast.in(data.obj.room_id).emit('alertback',{value:data.value,obj:data.obj});
+    });
+
+    socket.on('lightoff',function(data){
+      socket.broadcast.in(data.obj.room_id).emit('lightoffback',{obj:data.obj});
+    });
+
+    socket.on('leftroom',function(data){
+      var tempid="room-"+roomno;
+      if (tempid==data.obj.room_id) { roomno++; users=0; }
+      socket.broadcast.in(data.obj.room_id).emit('leftroomback',{obj:data.obj});
+    });
+
+});
+
+http.listen(3000, function() {
+   console.log('listening on *:3000');
+});
